4e7371f964049a6e0e59897d54247f3ef8f47b1c
wajidalikhi <aliw5592@gmail.com>
2025-02-08 16:47:22 +0500

    Re-uploading correct file

--- Q2_Hakathon_2/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['cdn.sanity.io'], // Add Sanity's CDN domain for image support
-  },
-};
-
-export default nextConfig;
--- Q2_Hakathon_2/src/sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from "sanity";
-import { productSchema } from "./products";
-import { categorySchema } from "./categories";
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [productSchema, categorySchema],
-};
